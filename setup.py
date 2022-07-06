cfd399e30b87260998b6961ec921cd050b2edb6a
Mazzie <mazzieola@hotmail.com>
2022-07-06 13:32:30 +0200

    folders

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='my-inusrance-scripts',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='An example python package',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/CT2-DEVOPS-Team2/myinsurancescripts.git',
+    author='Team2',
+    author_email='myemail@example.com'
+)
+
+
+# twine upload --repository-url http://localhost:8081/repository/hosted-python/ dist/*
