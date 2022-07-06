c23a89334a16245e61a811f637cf076dacfadd8f
Mazzie <mazzieola@hotmail.com>
2022-07-06 14:35:20 +0200

    twine

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my-inusrance-scripts',
+    name='my-insurance-scripts',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
