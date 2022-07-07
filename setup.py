75206527506b36833c5ce8df49b36f20f0832a19
Mazzie <mazzieola@hotmail.com>
2022-07-07 09:12:56 +0200

    new

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my-insurance-scripts',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='An example python package',
